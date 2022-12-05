ddeffca61d1aa8648fcdf8e9bd8f10c63b95aaf9
Michael <michaelfarquhar4@gmail.com>
2022-12-04 22:16:42 -0500

    Add basis for technical skills section

---
+++ src/skills.ts
@@ -0,0 +1,18 @@
+// group = Skills I've used at "work" or in "personal" projects.
+
+export default {
+    languages: [
+        {
+            src: 'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
+            title: 'HTML',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white',
+            title: 'CSS',
+            group: 'work',
+        },
+    ],
+    frameworks: [],
+    software: [],
+};

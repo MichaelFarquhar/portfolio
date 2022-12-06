44bee1b55e8f7b36a97fc36a00f7f67d3f378be0
Michael <michaelfarquhar4@gmail.com>
2022-12-05 22:49:50 -0500

    Add all skill markdown badges

--- src/skills.ts
+++ src/skills.ts
@@ -12,7 +12,114 @@ export default {
             title: 'CSS',
             group: 'work',
         },
+        {
+            src: 'https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white',
+            title: 'Sass',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E',
+            title: 'Javascript',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white',
+            title: 'JQuery',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white',
+            title: 'Typescript',
+            group: 'personal',
+        },
+        {
+            src: 'https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white',
+            title: 'PHP',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white',
+            title: 'MySQL',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue',
+            title: 'Python',
+            group: 'personal',
+        },
     ],
-    frameworks: [],
-    software: [],
+    frameworks: [
+        {
+            src: 'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
+            title: 'React',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white',
+            title: 'Redux',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/vuejs-%2335495e.svg?style=for-the-badge&logo=vuedotjs&logoColor=%234FC08D',
+            title: 'Vue',
+            group: 'personal',
+        },
+        {
+            src: 'https://img.shields.io/badge/laravel-%23FF2D20.svg?style=for-the-badge&logo=laravel&logoColor=white',
+            title: 'Laravel',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white',
+            title: 'Tailwind',
+            group: 'personal',
+        },
+        {
+            src: 'https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white',
+            title: 'Bootstrap',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase',
+            title: 'Firebase',
+            group: 'personal',
+        },
+    ],
+    software: [
+        {
+            src: 'https://img.shields.io/badge/VSCode-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white',
+            title: 'VS Code',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/Adobe%20XD-470137?style=for-the-badge&logo=Adobe%20XD&logoColor=#FF61F6',
+            title: 'Adobe XD',
+            group: 'personal',
+        },
+        {
+            src: 'https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white',
+            title: 'Git',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white',
+            title: 'Github',
+            group: 'personal',
+        },
+        {
+            src: 'https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white',
+            title: 'Heroku',
+            group: 'work',
+        },
+        {
+            src: 'https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7',
+            title: 'Netlify',
+            group: 'personal',
+        },
+        {
+            src: 'https://img.shields.io/badge/Notion-%23000000.svg?style=for-the-badge&logo=notion&logoColor=white',
+            title: 'Notion',
+            group: 'personal',
+        },
+    ],
 };

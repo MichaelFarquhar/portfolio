e9426a57e9323bb32b9cffe6550c2e3d13d44920
Michael <michaelfarquhar4@gmail.com>
2022-12-10 19:34:02 -0500

    Change all instances for template designer's name

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Michael Farquhar';
+export const SITE_TITLE = 'Michael Farquhar | Portfolio';
 export const SITE_DESCRIPTION = 'My portfolio website.';

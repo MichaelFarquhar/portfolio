5336a048b2e72e65cc87ca61975d67de552a4e39
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-02-22 16:12:25 -0500

    Add astro-icon and replace heroicons globally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
 import partytown from '@astrojs/partytown';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://portfolio.mikefarquhar.net/dist',
-    integrations: [
-        mdx(),
-        tailwind(),
-        partytown({
-            config: {
-                forward: ['dataLayer.push'],
-            },
-        }),
-    ],
+  site: 'https://portfolio.mikefarquhar.net/dist',
+  integrations: [mdx(), tailwind(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), icon()]
 });
